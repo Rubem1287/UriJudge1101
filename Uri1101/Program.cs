f68e459c7364d0da6281a7df17923999b60d9fac
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:13:40 -0300

    Add files via upload

---
+++ Uri1101/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Uri1101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior, menor, soma, a, b, sair;
+
+            soma = 0;
+            sair = 0;
+            maior = 1;
+            menor = 1;
+
+
+
+            while (sair !=1)
+            {
+
+                string[] vetor = Console.ReadLine().Split(' ');
+                a = int.Parse(vetor[0]);
+                b = int.Parse(vetor[1]);
+
+                if (a <= 0 || b <= 0)
+                {
+                    sair = 1;
+                }
+                else
+                {
+                    if (a > b)
+                    {
+                        maior = a;
+                        menor = b;
+                    }
+                    else
+                    {
+                        maior = b;
+                        menor = a;
+                    }
+
+
+
+                    for (int c = menor; c <= maior; c++)
+                    {
+
+
+                        soma = soma + c;
+                        Console.Write(c + " ");
+
+                    }
+
+
+
+                    Console.WriteLine("Sum = " + soma);
+                    soma = 0;
+                }
+            }
+        }
+    }
+}
